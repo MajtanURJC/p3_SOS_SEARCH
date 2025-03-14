c18192395b6a2787f14a0584b709b4e63a0b7219
majtan <ma.novo.2023@alumnos.urjc.es>
2025-03-14 13:58:28 +0100

    Añadir el programa terminado

--- searchstr.c
+++ searchstr.c
@@ -5,49 +5,79 @@
 #include <string.h>
 #include <err.h>
 
-struct Entrada{
+struct Entrada {
     int pid;
-    char* ruta;
+    char *ruta;
     char *palabra; 
 };
 
 typedef struct Entrada Entrada;
 
-int
-main(int argc, char *argv[]){
+struct Procesos {
+    Entrada *array;
+    int capacidad;
+};
+
+typedef struct Procesos Procesos;
+
+int main(int argc, char *argv[]) {
     int i = 0;
     int sts;
     int pid;
+    int num = 0;
 
     argc--;
     argv++;
 
-    if(argc == 0){
-        fprintf(stderr,"Nos has introducido ninguna dirección");
-    } else if(argc % 2 != 0){
-        printf("Número de parametros incorrecto");
+    Procesos* procesos = malloc(sizeof(Procesos));
+    procesos->capacidad = argc;
+
+    procesos->array = malloc(procesos->capacidad * sizeof(Entrada));
+    if (argc == 0) {
+        fprintf(stderr, "No has introducido ninguna dirección\n");
+        exit(EXIT_FAILURE);
+    } else if (argc % 2 != 0) {
+        fprintf(stderr, "Número de parámetros incorrecto\n");
         exit(EXIT_FAILURE);
     }
 
-    for(i = 0; i < argc ; i += 2){
+    for (i = 0; i < argc; i += 2) {
         pid = fork();
-        switch pid {
+        switch (pid) {
             case -1:
+                err(EXIT_FAILURE, "fork failed!");
+                break;
 
+            case 0:
+                execl("/usr/bin/fgrep", "fgrep", "-q", "-s", argv[i], argv[i + 1], NULL);
+                break;
+
+            default:
+                procesos->array[num].ruta = argv[i];
+                procesos->array[num].palabra = argv[i + 1];
+                procesos->array[num].pid = pid;
+                num++;
+                break;
         }
-        Entrada* entrada = malloc(sizeof(Entrada));
-        entrada->pid = tmp;
-        entrada->ruta = argv[i];
-        entrada->palabra = argv[i+1];
-        execl("/usr/bin/fgrep","fgrep","-q",entrada->direccion,entrada->palabra,NULL);
-    }
-    while ((pid = wait(&sts)) != -1) {
-        if(WIFEXITED(sts) == 0) {
-            printf("%s <%s>: yes\n",entrada->ruta,entrada->valor);
-        } else {
-            printf("%s <%s>: no\n",entrada->ruta,entrada->valor);
-        }    
     }
 
-    exit(EXIT_SUCCESS);
-}   
+    while ((pid = wait(&sts)) != -1) {
+        int found = 0;
+        for (i = 0; i < num; i++) {
+            if (procesos->array[i].pid == pid) {
+                found = 1;
+                if (WIFEXITED(sts) && WEXITSTATUS(sts) == 0) {
+                    printf("%s <%s>: yes\n", procesos->array[i].ruta, procesos->array[i].palabra);
+                } else {
+                    printf("%s <%s>: no\n", procesos->array[i].ruta, procesos->array[i].palabra);
+                }
+                break;
+            }
+        }
+    }
+
+    free(procesos->array);
+    free(procesos);
+
+    return 0;
+}
